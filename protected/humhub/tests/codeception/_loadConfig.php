369e995b916de536cfc4e2e55a6d610527afbbcc
yuri.mayorov <yuri.mayorov@aramcoinnovations.com>
2025-05-23 17:23:06 +0300

    Removed unneccessary comments

--- protected/humhub/tests/codeception/_loadConfig.php
+++ protected/humhub/tests/codeception/_loadConfig.php
@@ -1,10 +1,6 @@
 <?php
 
-/*
- * @link      https://www.humhub.org/
- * @copyright Copyright (c) 2023 HumHub GmbH & Co. KG
- * @license   https://www.humhub.com/licences
- */
+
 
 /**
  * This file is executed before the _bootstrap file and loads the humhub test config
